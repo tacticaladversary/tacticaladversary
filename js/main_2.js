118ce6488614cbba4b1d0f607a96a8e163c9c14b
Abhijith <14778039+abhijithbr@users.noreply.github.com>
2020-07-08 00:52:09 +0530

    Update main_2.js

--- js/main_2.js
+++ js/main_2.js
@@ -73,7 +73,29 @@
    });
 
 
-  
+     /*----------------------------------------------------*/
+	/* Adjust Primary Navigation Background Opacity
+	------------------------------------------------------*/
+   $(window).on('scroll', function() {
+
+		var h = $('header').height();
+		var y = $(window).scrollTop();
+      var header = $('#main-header');
+
+	   if ((y > h + 30 ) && ($(window).outerWidth() > 768 ) ) {
+	      header.addClass('opaque');	      
+	   }
+      else {
+         if (y < h + 30) {
+            header.removeClass('opaque');
+         }
+         else {
+            header.addClass('opaque');
+         }
+      }
+
+	});
+
 
 
    /*----------------------------------------------------*/
@@ -103,7 +125,7 @@
 
 	});
 
--*/
+-
    /*----------------------------------------------------*/
   	/* FitText Settings
   	------------------------------------------------------ */
